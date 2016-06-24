b58986647cc29d92a1fc01588f21ff62b7286739
Peter Tang <petersktang@gmail.com>
2016-06-24 21:52:26 +0800

    correct config location and fix build.gradle

--- bin/one-usa-gov-stream-consumer.sh
+++ bin/one-usa-gov-stream-consumer.sh
@@ -5,4 +5,4 @@ CONFIG_FILE='config/one-usa-gov.stream-consumer.properties'
 LOG4J_FILE='config/one-usa-gov.stream-consumer.log4j.properties'
 BASE_DIR=$(dirname $0)
 
-java -Dlog4j.configuration=file:$BASE_DIR/../config/$LOG4J_FILE -jar $BASE_DIR/../$JAR_FILE $BASE_DIR/../$CONFIG_FILE
+java -Dlog4j.configuration=file:$BASE_DIR/../$LOG4J_FILE -jar $BASE_DIR/../$JAR_FILE $BASE_DIR/../$CONFIG_FILE
